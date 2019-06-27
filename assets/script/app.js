60c094b47f890a907b4c7e0dfafe24973bd05ab7
Moumix <mouna.aitichou@gmail.com>
2019-06-26 22:50:13 -0400

    fixed bug where the counter doesn't stop after the user clicks submit

--- assets/script/app.js
+++ assets/script/app.js
@@ -7,48 +7,48 @@ var unanswered = 0;
 
 // Questions array
 var questions = [{
-     
+
     question: "What is the diameter of Earth?",
     answerList: ["4,000", "6,000", "9,000", "8,000"],
     answer: 3
-},{
+}, {
     question: "\"What is the capital of Canada?",
     answerList: ["Toronto", "Ottawa", "Montreal", "Vancouver"],
     answer: 1
-},{
+}, {
     question: "In computing history, who were the Dirty Dozen?",
     answerList: ["Shockley engineers", "I don't know", "The Internet's first infamous hacking group", "Engineers who created the first IBM PC"],
     answer: 3
-},{
+}, {
     question: "Before being known as PayPal, the company went by what name?",
     answerList: ["Confinity", "The X-Change", "MoneyMate", "iCash.com"],
     answer: 0
-},{
+}, {
     question: "Which of these companies invented Ethernet?",
     answerList: ["Cisco", "Xerox", "AT&T", "Packard Bell"],
     answer: 1
-},{
+}, {
     question: "What was the Mac's first web browser?",
     answerList: ["Cello", "Mosaic", "Samba", "ViolaWWW"],
     answer: 2
-},{
+}, {
     question: "Who launched the very first website?",
     answerList: ["CERN", "Stanford", "Xerox", "MIT"],
     answer: 0
 }];
 
-$("#start").on("click", function() {
+$("#start").on("click", function () {
     // Display initial time countdown
     $("#time").html("<h2>Time Remaining: 30 Seconds</h2>" + "<br>");
 
-      // Hide Start button
-      $(this).hide();
+    // Hide Start button
+    $(this).hide();
 
     // Start timer countdown
     run();
-   
+
     // Display questions
-    
+
     // Question 1
     $("#question1").html("<h3>" + questions[0].question + "</h3>");
     $("#answer1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answerList[0] + "</label>"
@@ -107,14 +107,16 @@ $("#start").on("click", function() {
     $("#submit").html("<button id='done' class='btn'>Done</button>");
 
     // Click event runs keepingScore() and displayResults() when user clicks Done button
-    $("#done").on("click", function() {
+    $("#done").on("click", function () {
 
         // Keeping track of score based on correct, incorrect, and unanswered
+        stop();
+
         keepingScore();
 
         // Display 
         displayResults();
-        
+
     });
 });
 
@@ -130,6 +132,7 @@ function decrement() {
 
     //  Decrease number by one.
     number--;
+
 
     //  Show the number in the #time tag
     $("#time").html("<h2>Time Remaining: " + number + " Seconds</h2>" + "<br>");
@@ -284,6 +287,6 @@ function keepingScore() {
     else {
 
         incorrectAnswers++;
-    }  
+    }
 }
 
